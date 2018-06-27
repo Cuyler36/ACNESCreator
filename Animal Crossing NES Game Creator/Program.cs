525f262597bcef0ba363b63ac42b9ab05c50e239
Cuyler36 <jeremy96@vt.edu>
2018-06-26 22:55:45 -0400

    Add project files.

---
+++ Animal Crossing NES Game Creator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Animal_Crossing_NES_Game_Creator
+{
+    class Program
+    {
+        internal static void SetChecksum(ref byte[] Data)
+        {
+            byte Checksum = 0;
+            for (int i = 0x40; i < Data.Length; i++) // Skip the header by adding 0x40
+            {
+                Checksum += Data[i];
+            }
+
+            Data[Data.Length - 1] = (byte)-Checksum;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("Enter the name of the NES game: ");
+                args = new string[1] { Console.ReadLine() };
+            }
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Enter the path to the NES game to inject: ");
+                args = new string[2] { args[0], Console.ReadLine().Replace("\"", "") };
+            }
+
+            if (args[0].Length >= 4 && File.Exists(args[1]))
+            {
+                byte[] NESData = File.ReadAllBytes(args[1]);
+                if (Encoding.ASCII.GetString(NESData, 0, 3) == "NES")
+                {
+                    // Pad name with spaces
+                    string Name = args[0];
+                    if (Name.Length < 0x10)
+                    {
+                        for (int i = Name.Length; i < 0x10; i++)
+                        {
+                            Name += (char)(0x20);
+                        }
+                    }
+
+                    byte[] ACData = new byte[NESData.Length + 0x60];
+                    byte[] NameData = Encoding.ASCII.GetBytes("ZZ" + Name);
+
+                    Array.Resize(ref NameData, 0x12);
+
+                    NameData.CopyTo(ACData, 0x40);
+                    // Copy Game Size
+                    ushort ResizedData = (ushort)(NESData.Length >> 4);
+                    BitConverter.GetBytes(ResizedData.Reverse()).CopyTo(ACData, 0x52);
+
+                    // Copy the NES ROM Data
+                    NESData.CopyTo(ACData, 0x60);
+
+                    // Create a new Blank GCI File
+                    GCI NESGCIFile = new GCI
+                    {
+                        Comment2 = args[0] + " NES Game",
+                        Data = ACData
+                    };
+                    // Set the File Name
+                    NESGCIFile.Header.FileName = "DobutsunomoriP_F_" + args[0].Substring(0, 4).ToUpper();
+
+                    // Write the newly created GCI file
+                    string OutputFile = Path.GetDirectoryName(args[1]) + "\\" + args[0] + "_InjectedData.gci";
+                    using (var Stream = new FileStream(OutputFile, FileMode.OpenOrCreate))
+                    {
+                        byte[] Data = NESGCIFile.GetData();
+                        SetChecksum(ref Data);
+                        Stream.Write(Data, 0, Data.Length);
+                    }
+
+                    Console.WriteLine("Successfully generated a GCI file with the NES rom in it!\nFile Location: " + OutputFile);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("The NES name must be four or more characters long!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Animal Crossing NES Game Creator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Animal Crossing NES Game Creator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Animal Crossing NES Game Creator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d04dc5ab-f68e-4c1d-8ea3-6dd782d3f3d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
