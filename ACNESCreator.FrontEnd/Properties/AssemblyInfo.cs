6e696d086032a89d51e52893319496ba7e720d1d
Cuyler36 <jeremy96@vt.edu>
2018-07-27 12:15:43 -0400

    Change FrontEnd assembly name

--- ACNESCreator.FrontEnd/Properties/AssemblyInfo.cs
+++ ACNESCreator.FrontEnd/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ACNESCreator.FrontEnd")]
+[assembly: AssemblyTitle("ACNESCreator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ACNESCreator.FrontEnd")]
+[assembly: AssemblyProduct("ACNESCreator")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
